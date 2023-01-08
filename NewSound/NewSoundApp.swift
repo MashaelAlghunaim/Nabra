09506973a852b6ee21bc563c029b72117012d2e0
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-01-08 15:45:20 +0300

    Initial Commit

---
+++ NewSound/NewSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewSoundApp.swift
+//  NewSound
+//
+//  Created by Mashael Alghunaim on 15/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct NewSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
