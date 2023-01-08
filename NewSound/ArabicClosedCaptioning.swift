22de27ed867351e1f2f6882d15a9848af1774e89
Mashael Alghunaim <mashael.alghunaim1@gmail.com>
2023-01-08 17:16:38 +0300

    .

---
+++ NewSound/ArabicClosedCaptioning.swift
@@ -0,0 +1,94 @@
+//
+//  ArabicClosedCaptioning.swift
+//  NewSound
+//
+//  Created by Mashael Alghunaim on 15/06/1444 AH.
+//
+
+import Foundation
+import Speech
+import AVFoundation
+
+
+class ArabicClosedCaptioning: ObservableObject{
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "ar-SA"))!
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+    @Published var captioning: String =  "كُلي آذانٌ صاغية"
+        
+    @Published var isPlaying: Bool = false
+    func startRecording() throws {
+            
+            // Cancel the previous task if it's running.
+            recognitionTask?.cancel()
+            self.recognitionTask = nil
+            
+            // Configure the audio session for the app.
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.record, mode: .measurement, options: .duckOthers)
+            try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+            let inputNode = audioEngine.inputNode
+
+            // Create and configure the speech recognition request.
+            recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+            guard let recognitionRequest = recognitionRequest else { fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object") }
+            recognitionRequest.shouldReportPartialResults = true
+            
+            // Keep speech recognition data on device
+            if #available(iOS 13, *) {
+                recognitionRequest.requiresOnDeviceRecognition = false
+            }
+            
+            // Create a recognition task for the speech recognition session.
+            // Keep a reference to the task so that it can be canceled.
+            recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+                var isFinal = false
+                
+                if let result = result {
+                    // ***We will update State here!***
+                    // ??? = result.bestTranscription.formattedString
+                    isFinal = result.isFinal
+                   // print("Text \(result.bestTranscription.formattedString)")
+                    
+                    // Update the text view with the results.
+                    self.captioning = result.bestTranscription.formattedString
+                }
+                
+                if error != nil || isFinal {
+                    // Stop recognizing speech if there is a problem.
+                    self.audioEngine.stop()
+                    inputNode.removeTap(onBus: 0)
+
+                    self.recognitionRequest = nil
+                    self.recognitionTask = nil
+                }
+            }
+
+            // Configure the microphone input.
+            let recordingFormat = inputNode.outputFormat(forBus: 0)
+            inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+                self.recognitionRequest?.append(buffer)
+            }
+            
+            audioEngine.prepare()
+            try audioEngine.start()
+        }
+    
+    func micButtonTapped(){
+      if audioEngine.isRunning {
+        audioEngine.stop()
+        recognitionRequest?.endAudio()
+        isPlaying = false
+      } else {
+        do {
+          try startRecording()
+          isPlaying = true
+        } catch {
+        isPlaying = false
+        }
+    }}
+    
+    
+}
--- NewSound/ContentView.swift
+++ NewSound/ContentView.swift
@@ -6,18 +6,99 @@
 //
 
 import SwiftUI
-
+import Speech
 struct ContentView: View {
+    @State var pickedTheme = "1"
+    @State private var animateAquaColor = false
+    @State private var animateSkyColor = false
+    @ObservedObject var closedCap = ClosedCaptioning()
+    @ObservedObject var arabicClosedCap = ArabicClosedCaptioning()
+    @State private var micEnabled: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color("darkBlue")
+                .ignoresSafeArea()
+            VStack{
+                Text("اختر اللغة المراد عرضها:").foregroundColor(Color.white)
+                    .font(.system(size: 20))
+                    .padding(.bottom, 700.0)
+                    .padding(.leading,160.0)
+                
+                
+            }
+            
+            Picker(" ", selection: $pickedTheme, content: {
+                Text("العربية").tag("1")
+                    .foregroundColor(.gray)
+                Text("الإنجليزية").tag("2")
+                
+                
+            }).padding(.bottom, 600.0).pickerStyle(.segmented)
+                .padding(.horizontal)
+            
+           // Divider()
+            
+            
+            VStack {
+                if pickedTheme == "2"{
+                    Text(self.closedCap.captioning).foregroundColor(Color.white)
+                        .font(.system(size: 30))
+                    .padding(.top, 280)}
+                else{
+                    Text(self.arabicClosedCap.captioning).foregroundColor(Color.white)
+                            .font(.system(size: 30))
+                        .padding(.top, 280)}
+                }
+                if pickedTheme == "2"{
+                    Button( action: {
+                        self.closedCap.micButtonTapped() })
+                    {
+                        ZStack{
+                            Circle()
+                                .frame(width: 200, height: 200, alignment: .bottomLeading)
+                                .foregroundColor(Color("veryMediumBlue"))
+                                .scaleEffect(animateSkyColor ? 1 : 0.8)
+                                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).speed(2), value: animateSkyColor)
+                            Circle()
+                                .frame(width: 120, height: 120, alignment: .bottomLeading)
+                                .foregroundColor(Color("mediumblue"))
+                                .scaleEffect(animateSkyColor ? 1 : 1.5)
+                                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).speed(3), value: animateAquaColor)
+                            
+                            Image (systemName: "ear")
+                                .font(.system(size: 100))
+                                .foregroundColor(.white)
+                                .frame(width: 200, height: 200)
+                        }
+                    }
+                    
+                }
+                else{
+                    Button( action: {
+                        self.arabicClosedCap.micButtonTapped() })
+                    {
+                        ZStack{
+                            Circle()
+                                .frame(width: 200, height: 200, alignment: .bottomLeading)
+                                .foregroundColor(Color("veryMediumBlue"))
+                                .scaleEffect(animateSkyColor ? 1 : 0.8)
+                                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).speed(2), value: animateSkyColor)
+                            Circle()
+                                .frame(width: 120, height: 120, alignment: .bottomLeading)
+                                .foregroundColor(Color("mediumblue"))
+                                .scaleEffect(animateSkyColor ? 1 : 1.5)
+                                .animation(Animation.easeInOut(duration: 0.5).repeatForever(autoreverses: true).speed(3), value: animateAquaColor)
+                            
+                            Image (systemName: "ear")
+                                .font(.system(size: 100))
+                                .foregroundColor(.white)
+                                .frame(width: 200, height: 200)
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
